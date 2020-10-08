adb626fc539f932986cc646d3896bb116876801b
Worek Dawid <dawid.worek@aalto.fi>
2020-10-08 22:05:16 +0300

    Some improvements for the script

--- handovers_finder.py
+++ handovers_finder.py
@@ -1,4 +1,6 @@
+import sys
 import csv
+import os
 
 class CellMapperRow:
     def __init__(self, csv_row):
@@ -7,9 +9,12 @@ class CellMapperRow:
         self.arfcn = None
         self.psc_or_pci = None
         if len(csv_row) > 10:
-            self.arfcn = csv_row[10]
-            if len(csv_row) > 11:
-                self.psc_or_pci = csv_row[11]
+            try:
+                self.arfcn = csv_row[10]
+                if len(csv_row) > 11:
+                    self.psc_or_pci = csv_row[11]
+            except ValueError:
+                pass
 
 def parse_signal_csv(filename):
     cellmapper_data = []
@@ -20,9 +25,12 @@ def parse_signal_csv(filename):
                 int(row[3]), int(row[4]), int(row[5]), int(row[6]), int(row[7]), \
                 row[8], row[9]]
             if len(row) > 10:
-                row_parsed.append(int(row[10]))
-                if len(row) > 11:
-                    row_parsed.append(int(row[11]))
+                try:
+                    row_parsed.append(int(row[10]))
+                    if len(row) > 11:
+                        row_parsed.append(int(row[11]))
+                except ValueError:
+                    pass
             cellmapper_data.append(CellMapperRow(row_parsed))
 
     return cellmapper_data
@@ -33,24 +41,27 @@ def get_rows_diff(row_one, row_two):
         row_two.signal - row_one.signal, row_one.type + "->" + row_two.type, \
         row_one.subtype + "->" +  row_two.subtype, str(row_one.arfcn) + "->" + str(row_two.arfcn))
 
-def find_handovers(cellmapper_data):
-    # TODO smarter output file name
-    output_file = open("out.csv", mode='w', newline='')
+def find_handovers(cellmapper_data, output_filename="out.csv"):
+    output_file = open(output_filename, mode='w', newline='')
     csv_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     row_one = cellmapper_data[0]
+    mnc = row_one.mnc
     for i in range(1, len(cellmapper_data)):
         row_two = cellmapper_data[i]
 
-        # TODO add multi MNC support (dual sim)
-        # TODO prune data and recognize situation when data is bullshit (like extremely high RSSI)
+        # TODO add full multi MNC support (dual sim)
         # TODO automatically detect more insteresting facts
-        if row_one.cid != row_two.cid:
+        if row_one.cid != row_two.cid and \
+            (row_one.signal < -51 and row_two.signal < -51) and \
+            (row_one.mnc == mnc and row_two.mnc == mnc):
             csv_writer.writerow(get_rows_diff(row_one, row_two))
         
         row_one = row_two
 
     output_file.close()
-    print("out.csv genereted")
+    print("{} genereted".format(output_filename))
 
 if __name__ == "__main__":
-    find_handovers( parse_signal_csv("../signal-2020-09-25_Thomas.csv") )
+    input_filename = sys.argv[1]
+    output_filename = os.path.basename(input_filename).split('.')[0] + '_handovers.csv'
+    find_handovers(parse_signal_csv(input_filename), output_filename)
